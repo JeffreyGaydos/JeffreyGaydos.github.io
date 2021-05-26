feca180386cf485aa847589d28865fdfd7283e68
JeffreyGaydos <jeff13gaydos@gmail.com>
2021-05-26 18:59:31 -0400

    Removed files creating build issues with react

---
+++ build/precache-manifest.2b9f3150f19cf861e94bf28b4e1f7ba1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f940b8e97639796366c7a305186ed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9d4c3c89ec6252bc8f",
+    "url": "/static/css/main.1e8a6d88.chunk.css"
+  },
+  {
+    "revision": "f626ebff0e6cf20b3b3a",
+    "url": "/static/js/2.5907aee1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5907aee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9d4c3c89ec6252bc8f",
+    "url": "/static/js/main.af275603.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "31f52d48fb7d05405d24dfa88b2177fe",
+    "url": "/static/media/AvenirNextLTPro-UltLt.31f52d48.otf"
+  },
+  {
+    "revision": "7db45720bf1fcc84e7726c091521c66c",
+    "url": "/static/media/project1tanksicon.7db45720.PNG"
+  },
+  {
+    "revision": "11527124ecfa19ebd2e3dd4f74ba11d8",
+    "url": "/static/media/project4toolset.11527124.PNG"
+  }
+]);
