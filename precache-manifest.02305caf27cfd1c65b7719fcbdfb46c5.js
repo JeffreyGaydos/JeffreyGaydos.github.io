31cb37d7aecacb1f0891ff50f20ec2cf9b15254c
JeffreyGaydos <jeff13gaydos@gmail.com>
2021-05-26 14:19:47 -0400

    Updates

---
+++ precache-manifest.02305caf27cfd1c65b7719fcbdfb46c5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9308e8ff363c89c9adb805af0beb5fd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "615c5d4b41c0f4614061",
+    "url": "/static/css/main.e063a2a0.chunk.css"
+  },
+  {
+    "revision": "1d1c356510b28a77f027",
+    "url": "/static/js/2.4e8c15f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4e8c15f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615c5d4b41c0f4614061",
+    "url": "/static/js/main.859bad51.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "31f52d48fb7d05405d24dfa88b2177fe",
+    "url": "/static/media/AvenirNextLTPro-UltLt.31f52d48.otf"
+  },
+  {
+    "revision": "7db45720bf1fcc84e7726c091521c66c",
+    "url": "/static/media/project1tanksicon.7db45720.PNG"
+  },
+  {
+    "revision": "11527124ecfa19ebd2e3dd4f74ba11d8",
+    "url": "/static/media/project4toolset.11527124.PNG"
+  }
+]);
